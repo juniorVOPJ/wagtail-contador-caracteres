3477001e6140bd9d36ac44681f98fd8111c9ea98
Seb <seb@neonjungle.studio>
2023-12-30 12:50:15 +1100

    up min wagtail version, up version

--- setup.py
+++ setup.py
@@ -3,37 +3,33 @@
 Install wagtail-charcount using setuptools
 """
 
-with open('README.md', 'r') as f:
+with open("README.md", "r") as f:
     readme = f.read()
 
 from setuptools import find_packages, setup
 
 setup(
-    name='wagtail-charcount',
-    version='0.1.2',
+    name="wagtail-charcount",
+    version="0.2.0",
     description="A wagtail character and word counting plugin for RichTextFields.",
     long_description=readme,
-    author='Liam Brenner',
-    author_email='liam@takeflight.com.au',
-    url='https://github.com/takeflight/wagtail-charcount',
-
+    author="Liam Brenner",
+    author_email="liam@takeflight.com.au",
+    url="https://github.com/takeflight/wagtail-charcount",
     install_requires=[
-        'wagtail>=1.0',
+        "wagtail>=4.1",
     ],
     zip_safe=False,
-    license='BSD License',
-
+    license="BSD License",
     packages=find_packages(),
-
     include_package_data=True,
     package_data={},
-
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Framework :: Django',
-        'License :: OSI Approved :: BSD License',
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
     ],
 )
